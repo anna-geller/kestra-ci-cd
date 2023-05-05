358520c7f48417d9be4c77b327ac15419bc4d7ca
Anna Geller <anna.m.geller@gmail.com>
2023-05-05 12:46:36 +0200

    basic repo structure to maintain ci/cd with tf cloud

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    kestra = {
+      source = "kestra-io/kestra" # namespace of Kestra provider
+      version = "~> 0.7.0"
+    }
+  }
+}
+
+provider "kestra" {
+  url = "https://demo.kestra.io"
+  username = var.username
+  password = var.password
+#  jwt = "your.jwt.token"
+}
+
+resource "kestra_flow" "prod" {
+  for_each = fileset(path.module, "flows/*.yml")
+  flow_id = yamldecode(templatefile(each.value, {}))["id"]
+  namespace = yamldecode(templatefile(each.value, {}))["namespace"]
+  content = templatefile(each.value, {})
+}
+
+resource "kestra_flow" "prod_marketing" {
+  for_each = fileset(path.module, "flows/marketing/*.yml")
+  flow_id = yamldecode(templatefile(each.value, {}))["id"]
+  namespace = yamldecode(templatefile(each.value, {}))["namespace"]
+  content = templatefile(each.value, {})
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+    description = "User name of the Kestra instance"
+    type        = string
+}
+
+variable "password" {
+    description = "Password of the Kestra instance"
+    type        = string
+}
