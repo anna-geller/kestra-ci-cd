5729b7828f2974d093fb21461257c53aa70f2b48
Anna Geller <anna.m.geller@gmail.com>
2023-06-05 16:55:10 +0200

    tf dev and prod

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# kestra
 Example repository template for your flows
+
+```bash
+terraform import kestra_namespace.prod prod
+terraform apply -auto-approve -var-file prod.tfvar
+terraform destroy -var-file prod.tfvar
+```
+
--- main.tf
+++ main.tf
@@ -1,52 +1,56 @@
 terraform {
   required_providers {
     kestra = {
-      source = "kestra-io/kestra" # namespace of Kestra provider
+      source  = "kestra-io/kestra" # namespace of Kestra provider
       version = "~> 0.7.0"
     }
   }
 }
 
 provider "kestra" {
-  url = "https://demo.kestra.io"
+  url      = var.hostname
   username = var.username
   password = var.password
-#  jwt = "your.jwt.token"
 }
 
-resource "kestra_namespace" "com_kestra" {
-  namespace_id = "com.kestra"
-  variables = yamlencode({
-    "slack" = {
-      "webhook" = "{{ secret('SLACK_WEBHOOK') }}"
-    },
-    "github" = {
-      "token": "{{ secret('GITHUB_TOKEN') }}"
-    },
-    "maxmind" = {
-      apiKey = "{{ secret('MAXMIND_APIKEY') }}"
-    }
-  })
-  task_defaults = file("task-default.yml")
-}
-
-resource "kestra_flow" "flows" {
-  for_each = fileset(path.module, "flows/*.yml")
-  flow_id = yamldecode(templatefile(each.value, {}))["id"]
-  namespace = yamldecode(templatefile(each.value, {}))["namespace"]
-  content = templatefile(each.value, {})
+resource "kestra_namespace" "prod" {
+  count         = var.env == "prod" ? 1 : 0
+  namespace_id  = var.env
+  task_defaults = file("taskDefaults.yml")
 }
 
 resource "kestra_namespace_secret" "slack_webhook" {
-  namespace = "prod"
-  secret_key = "SLACK_WEBHOOK"
+  count        = var.env == "prod" ? 1 : 0
+  namespace    = "prod"
+  secret_key   = "SLACK_WEBHOOK"
   secret_value = var.slack_webhook
 }
 
-resource "kestra_template" "templates" {
-  for_each = fileset(path.module, "templates/*.yml")
-  template_id = yamldecode(templatefile(each.value, {}))["id"]
+resource "kestra_namespace_secret" "gcp_project_id" {
+  count        = var.env == "prod" ? 1 : 0
+  namespace    = "prod"
+  secret_key   = "GCP_PROJECT_ID"
+  secret_value = var.gcp_project_id
+}
+
+resource "kestra_namespace_secret" "gcp_service_account" {
+  count        = var.env == "prod" ? 1 : 0
+  namespace    = "prod"
+  secret_key   = "GCP_SERVICE_ACCOUNT"
+  secret_value = var.gcp_project_id
+}
+
+resource "kestra_flow" "flows" {
+  for_each  = fileset(path.module, "flows/*.yml")
+  flow_id   = yamldecode(templatefile(each.value, {}))["id"]
   namespace = yamldecode(templatefile(each.value, {}))["namespace"]
-  content = templatefile(each.value, {})
-  depends_on = [kestra_flow.flows] # Here we ensure that the flow is deployed before the template
+  content   = templatefile(each.value, {})
+}
+
+resource "kestra_template" "templates" {
+  for_each    = fileset(path.module, "templates/*.yml")
+  template_id = yamldecode(templatefile(each.value, {}))["id"]
+  namespace   = yamldecode(templatefile(each.value, {}))["namespace"]
+  content     = templatefile(each.value, {})
+  depends_on  = [kestra_flow.flows] # Here we ensure that the flow is deployed before the template
 }
