9886bd507a613282e2461aead32c7a8c3de24bd3
Anna Geller <anna.m.geller@gmail.com>
2023-11-30 13:14:42 +0100

    namespace files

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     kestra = {
       source  = "kestra-io/kestra"
-      version = "~>0.11.1"
+      version = "~>0.13"
     }
   }
 }
@@ -24,4 +24,11 @@ resource "kestra_flow" "flows" {
   flow_id              = yamldecode(templatefile(each.value, {}))["id"]
   namespace            = yamldecode(templatefile(each.value, {}))["namespace"]
   content              = templatefile(each.value, {})
+}
+
+resource "kestra_namespace_file" "prod_scripts" {
+  for_each  = fileset(path.module, "scripts/**")
+  namespace = "prod"
+  filename = "/${each.value}"
+  content   = file(each.value)
 }
